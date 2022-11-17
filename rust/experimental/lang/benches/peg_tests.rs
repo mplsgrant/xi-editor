15ba1dd46e22fffe4a41c29858a28448f4470c9b
mpls <mpls@posteo.net>
2022-11-16 22:19:35 -0600

    Swapped in Criterion in bench tests.
    Kind of mess in the experimental section, which makes sense. Going to
    allow some things to remain commented out, and removed some items
    related to testing within the bench file.

--- rust/experimental/lang/benches/peg_tests.rs
+++ rust/experimental/lang/benches/peg_tests.rs
@@ -14,8 +14,6 @@
 
 //! Benchmarks of PEG parsing libraries
 
-#![feature(test)]
-
 /// Run as:
 /// ```
 /// run nightly cargo bench --features "nom regex pom"
@@ -24,8 +22,7 @@ use std::env;
 
 extern crate xi_lang;
 
-#[cfg(test)]
-extern crate test;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 #[cfg(feature = "pom")]
 extern crate pom;
@@ -42,50 +39,56 @@ extern crate combine;
 
 const TEST_STR: &str = "1.2345e56";
 
-#[cfg(all(test, feature = "pom"))]
-mod pom_benches {
-    use super::{test, TEST_STR};
-    use pom::parser::{one_of, sym};
-    use pom::{DataInput, Parser};
-    use test::Bencher;
+// #[cfg(all(test, feature = "pom"))]
+// mod pom_benches {
+//     use super::TEST_STR;
+//     use criterion::{black_box, criterion_group, criterion_main, Criterion};
+//     use pom::parser::{one_of, sym};
+//     use pom::{DataInput, Parser};
 
-    fn pom_number() -> Parser<u8, usize> {
-        let integer = one_of(b"123456789") - one_of(b"0123456789").repeat(0..) | sym(b'0');
-        let frac = sym(b'.') + one_of(b"0123456789").repeat(1..);
-        let exp = one_of(b"eE") + one_of(b"+-").opt() + one_of(b"0123456789").repeat(1..);
-        let number = sym(b'-').opt() + integer + frac.opt() + exp.opt();
-        number.pos()
-    }
+//     fn pom_number() -> Parser<u8, usize> {
+//         let integer = one_of(b"123456789") - one_of(b"0123456789").repeat(0..) | sym(b'0');
+//         let frac = sym(b'.') + one_of(b"0123456789").repeat(1..);
+//         let exp = one_of(b"eE") + one_of(b"+-").opt() + one_of(b"0123456789").repeat(1..);
+//         let number = sym(b'-').opt() + integer + frac.opt() + exp.opt();
+//         number.pos()
+//     }
 
-    #[bench]
-    fn bench_pom(b: &mut Bencher) {
-        let parser = pom_number();
+//     fn bench_pom(c: &mut Criterion) {
+//         let parser = pom_number();
 
-        b.iter(|| {
-            let mut buf = DataInput::new(test::black_box(TEST_STR.as_bytes()));
-            parser.parse(&mut buf)
-        })
-    }
-}
+//         c.bench_function("bench_pom", |b| {
+//             b.iter(|| {
+//                 let mut buf = DataInput::new(test::black_box(TEST_STR.as_bytes()));
+//                 parser.parse(&mut buf)
+//             })
+//         });
+//     }
+
+//     criterion_group!(experimental_pom_benches, bench_pom);
+//     criterion_main!(experimental_pom_benches);
+// }
 
 #[cfg(all(test, feature = "regex"))]
 mod regex_benches {
-    use super::{test, TEST_STR};
+    use super::TEST_STR;
+    use criterion::{black_box, criterion_group, criterion_main, Criterion};
     use regex::Regex;
-    use test::Bencher;
 
-    #[bench]
-    fn bench_regex(b: &mut Bencher) {
+    fn bench_regex(c: &mut Criterion) {
         let re = Regex::new(r"^(0|[1-9][0-9]*)(\.[0-9]+)?([eE]([+-])?[0-9]+)?").unwrap();
-        b.iter(|| re.find(test::black_box(TEST_STR)))
+        c.bench_function("bench_regex", |b| b.iter(|| re.find(black_box(TEST_STR))));
     }
+
+    criterion_group!(experimental_regex_benches, bench_regex);
+    criterion_main!(experimental_regex_benches);
 }
 
 #[cfg(all(test, feature = "nom"))]
 mod nom_benches {
-    use super::{test, TEST_STR};
+    use super::TEST_STR;
+    use criterion::{black_box, criterion_group, criterion_main, Criterion};
     use nom::digit;
-    use test::Bencher;
 
     named!(digits<()>, fold_many1!(digit, (), |_, _| ()));
 
@@ -106,40 +109,46 @@ mod nom_benches {
     );
 
     #[cfg(feature = "nom")]
-    #[bench]
-    fn bench_nom(b: &mut Bencher) {
-        b.iter(|| nom_num(test::black_box(TEST_STR.as_bytes())))
+    fn bench_nom(c: &mut Criterion) {
+        c.bench_function("bench_nom", |b| b.iter(|| nom_num(black_box(TEST_STR.as_bytes()))));
     }
+
+    criterion_group!(experimental_nom_benches, bench_nom);
+    criterion_main!(experimental_nom_benches);
 }
 
-#[cfg(all(test, feature = "combine"))]
-mod combine_benches {
-    use super::{is_digit, test, TEST_STR};
-    use combine::range::take_while1;
-    use combine::*;
-    use test::Bencher;
+// #[cfg(all(test, feature = "combine"))]
+// mod combine_benches {
+//     use super::{is_digit, test, TEST_STR};
+//     use combine::range::take_while1;
+//     use combine::*;
+//     use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-    fn my_number(s: &[u8]) -> ParseResult<(), &[u8]> {
-        (
-            token(b'-').map(Some).or(value(None)),
-            token(b'0').map(|_| &b"0"[..]).or(take_while1(is_digit)),
-            optional((token(b'.'), take_while1(is_digit))),
-            optional((
-                token(b'e').or(token(b'E')),
-                token(b'-').map(Some).or(token(b'+').map(Some)).or(value(None)),
-                take_while1(is_digit),
-            )),
-        )
-            .map(|_| ())
-            .parse_stream(s)
-    }
+//     fn my_number(s: &[u8]) -> ParseResult<(), &[u8]> {
+//         (
+//             token(b'-').map(Some).or(value(None)),
+//             token(b'0').map(|_| &b"0"[..]).or(take_while1(is_digit)),
+//             optional((token(b'.'), take_while1(is_digit))),
+//             optional((
+//                 token(b'e').or(token(b'E')),
+//                 token(b'-').map(Some).or(token(b'+').map(Some)).or(value(None)),
+//                 take_while1(is_digit),
+//             )),
+//         )
+//             .map(|_| ())
+//             .parse_stream(s)
+//     }
 
-    #[bench]
-    fn bench_combine(b: &mut Bencher) {
-        assert_eq!(parser(my_number).parse(TEST_STR.as_bytes()), Ok(((), &b""[..])));
-        b.iter(|| parser(my_number).parse(test::black_box(TEST_STR.as_bytes())))
-    }
-}
+//     fn bench_combine(c: &mut Criterion) {
+//         assert_eq!(parser(my_number).parse(TEST_STR.as_bytes()), Ok(((), &b""[..])));
+//         c.bench_function("bench_combine", |b| {
+//             b.iter(|| parser(my_number).parse(test::black_box(TEST_STR.as_bytes())))
+//         });
+//     }
+
+//     criterion_group!(experimental_combine_benches, bench_combine);
+//     criterion_main!(experimental_combine_benches);
+// }
 
 use xi_lang::peg::{Alt, OneByte, OneOrMore, Optional, Peg};
 
@@ -157,26 +166,9 @@ fn my_number(s: &[u8]) -> Option<usize> {
         .p(s)
 }
 
-fn main() {
-    if let Some(s) = env::args().nth(1) {
-        println!("my: {:?}", my_number(s.as_bytes()));
-        /*
-        let mut buf = DataInput::new(s.as_bytes());
-        println!("pom: {:?}", pom_number().parse(&mut buf));
-        let re = Regex::new(r"^(0|[1-9][0-9]*)(\.[0-9]+)?([eE]([+-])?[0-9]+)?").unwrap();
-        println!("regex: {:?}", re.find(&s));
-        println!("nom: {:?}", nom_num(s.as_bytes()));
-        */
-    }
+fn bench_my_peg(c: &mut Criterion) {
+    c.bench_function("bench_my_peg", |b| b.iter(|| my_number(black_box(TEST_STR.as_bytes()))));
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use test::Bencher;
-
-    #[bench]
-    fn bench_my_peg(b: &mut Bencher) {
-        b.iter(|| my_number(test::black_box(TEST_STR.as_bytes())))
-    }
-}
+criterion_group!(experimental_my_peg_benches, bench_my_peg);
+criterion_main!(experimental_my_peg_benches);
